a4e1adb63c6169f5ec4f35043d0c096d43835739
shridharmanvi <shridharmanvi@gmail.com>
2014-11-28 02:35:46 -0600

    Added data cleansing steps

--- prog3.py
+++ prog3.py
@@ -1,9 +1,15 @@
 import re
-from nltk.corpus import stopwords #download nltk module by running the command 'sudo pip install -U nltk'
-#Import files 
-job=open('jobs.tsv','rw')
+import sys
+reload(sys)
 
-#cachedStopWords = stopwords.words('english') #caching stop words to speeden the process of removing stop words
+from nltk.corpus import stopwords #download nltk module by running the command 'sudo pip install -U nltk'
+from nltk.stem.lancaster import LancasterStemmer
+
+sys.setdefaultencoding("utf-8")
+
+#---------------------------------DATA Import-------------------------------------
+#Import files 
+job=open('/Users/shridharmanvi/Desktop/Projects/dm3/data/jobs.tsv','rw')
 
 jobs={}
 
@@ -15,16 +21,26 @@ for row in job.readlines():
     except ValueError:
         x=1
 
+#---------------------------------DATA CLEANSING-------------------------------------
+#1.Stopword removal
+#2.Word stemming
+
+cachedStopWords = stopwords.words('english') #caching stop words to speeden the process of removing them
 tags=re.compile(r'<[^>]+>')#<tags>
 backslash=re.compile(r'\\\w')
+utfremove=re.compile(r'[^\x00-\x7F]+')
+stemmer = LancasterStemmer()
 
+#the following loop removes html tags and stop words from 'Description' attribute from the data
 for k in jobs.keys():
-    rep=jobs[k][1]
+    rep=jobs[k][1] + ' ' + str(jobs[k][2])
     rep=tags.sub(' ',rep)
     rep=backslash.sub(' ',rep)
-    rep = ' '.join([word for word in rep.split() if word not in stopwords.words("english")])
+    rep=utfremove.sub(' ',rep)
+    rep = ' '.join([word for word in rep.split() if word not in cachedStopWords]) #and ord(word)<128])
+    rep = [stemmer.stem(word) for word in rep.split()]
     jobs[k][1]= rep
-    
 
-print jobs[965577][1]
+
+print jobs[741623][1]
 print len(jobs.keys())
